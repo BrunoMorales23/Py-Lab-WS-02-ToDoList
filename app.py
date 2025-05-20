4263283d5f2c8e16fd9c13d1524526fd8687e53f
Bruno Morales <brunomorales0323@outlook.es>
2025-05-19 23:28:51 -0300

    HTML Templates included

--- app.py
+++ app.py
@@ -3,5 +3,24 @@ from flask import Flask, render_template, request, abort, jsonify, redirect, url
 
 app = Flask(__name__)
 
+@app.route("/", methods=['POST','GET'])
+def main():
+    action = request.form.get("Click")
+    #oa = request.form.get("Prueba")
+    #jh = request.form.get("jh")
+    method = request.form.get("Prioridad")
+
+    print(action)
+#    print(oa)
+    print(method)
+#    print(jh)
+    return render_template('index.html')
+
+@app.route("/task", methods=['POST','GET'])
+def task():
+    action = request.form.get("button")
+    print(action)
+    return render_template('task.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
